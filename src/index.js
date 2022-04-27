99c9855572e258ed54df1cb22a6ed342c37341bf
Aleksey Belikov <aabelikoff@gmail.com>
2022-04-27 14:31:22 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import dataController from './dataController';
 import forms from './forms';
 import uiController from './uiController';
 
+
 function storageAvailable(type) {
 	try {
 		var storage = window[type],
